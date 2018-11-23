f1831a9deb9cc856fff05c98e973778720e1c27e
Ubuntu <leedain@fishing-phishingVM.33lkleynwp4uzbephgzfn2hhcc.xx.internal.cloudapp.net>
2018-11-23 17:18:05 +0000

    feat: code modifiy

--- start.js
+++ start.js
@@ -32,21 +32,55 @@ app.post('/text', function(req,res){
 
 
 
-  var spawn = require("child_process").spawn;
+  // var spawn = require("child_process").spawn;
 
-  var process = spawn('python',["./Deep_Learning/CNNTextClassification/eval.py",
+  // var process = spawn('python',["./Deep_Learning/CNNTextClassification/hello.py",
 
-                              voice_text] );
+  // ​                            voice_text] );
 
-  process.stdout.on('data', function(data) {
+  // ​process.stdout.on('data', function(data) {
 
-    res.send(data.toString());
+  //   res.send(data.toString());
 
-  })
-   
+  // })
 
-  res.send("ok");
-}); 
+  var PythonShell = require('python-shell');
+
+    
+
+  var options = {
+
+    mode: 'text',
+
+    pythonPath: './Deep_Learning/CNNTextClassification/',
+
+    pythonOptions: ['-u'],
+
+    scriptPath: '',
+
+    args: [voice_text]
+
+  };
+
+  
+
+  PythonShell.run('eval.py', options, function (err, results) {
+
+    if (err) 
+
+      throw err;
+
+    // Results is an array consisting of messages collected during execution
+
+    console.log('results: %j', results);
+
+  });
+
+  
+
+  
+
+});
 
 
 
