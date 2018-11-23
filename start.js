c5cefcb7cdcdee0d0e994da005531b54dc88171b
Ubuntu <leedain@fishing-phishingVM.33lkleynwp4uzbephgzfn2hhcc.xx.internal.cloudapp.net>
2018-11-23 11:00:50 +0000

    feat: error test

--- start.js
+++ start.js
@@ -26,23 +26,29 @@ app.post('/', function(req,res){
 
   if(initialize == 1)
 
-    voice_text = '';
+  voice_text = '';
 
   voice_text += req.body.text;
 
 
- res.send("voice text : " + voice_text.toString() + "\n");
+  res.send("voice text : " + voice_text.toString() + "\n");
 
   var spawn = require("child_process").spawn;
+
+  res.send("test1");
 
   var process = spawn('python',["./Deep_Learning/CNNTextClassification/eval.py",
                             voice_text] );
 
- process.stdout.on('data', function(data) {
+  res.send("test2");
+
+  process.stdout.on('data', function(data) {
       res.send(data.toString() + "\n");
     })
+  res.send("test3");
 
   res.end("python end!");
+  res.send("test4");
 });
 
 
