50e516490896c81778d25d6926d3c042992f0bfd
Ubuntu <leedain@fishing-phishingVM.33lkleynwp4uzbephgzfn2hhcc.xx.internal.cloudapp.net>
2018-11-23 11:19:57 +0000

    feat: code modifiy

--- start.js
+++ start.js
@@ -10,44 +10,54 @@ app.set('port', process.env.PORT || 3001);
 
 var bodyParser = require('body-parser');
 
+
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 
 
 
-var voice_text = '';
+app.post('/login',function(req,res){
+
+  var user_name=req.body.user;
+
+  var password=req.body.password;
+
+  console.log("User name = "+user_name+", password is "+password);
+
+  res.end("yes");
+
+});
 
 
 
-app.post('/', function(req,res){
-
-  var initialize = req.body.initialize;
-
-  if(initialize == 1)
-
-  voice_text = '';
-
-  voice_text += req.body.text;
 
 
-  var spawn = require("child_process").spawn;
+app.get('/name', function(req, res) {
 
-  var process = spawn('python',["./hello.py",
-                            voice_text,initialize ] );
+    var spawn = require("child_process").spawn;
+
+    var process = spawn('python',["./hello.py",
+
+                            req.query.firstname,
+
+                            req.query.lastname] );
 
 
-  process.stdout.on('data', function(data) {
-      res.send(data.toString() + "\n");
+
+    process.stdout.on('data', function(data) {
+
+      res.send(data.toString());
+
     })
-  res.send("test3");
 
-  res.send("test4");
 });
 
 
 
 http.createServer(app).listen(app.get('port'), function(){
+
 	console.log('start server on port' + app.get('port'));
 
 });
